80da7ef2fc17d995fe0c5c2d551ef7eae9e4ccd8
Kamil Lewiński <kml.lewinski3@gmail.com>
2025-11-25 10:11:30 +0100

    adsfsd

---
+++ electron/main.js
@@ -0,0 +1,38 @@
+// electron/main.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    backgroundColor: '#020617',
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  const devUrl = process.env.ELECTRON_START_URL;
+
+  if (devUrl) {
+    win.loadURL(devUrl);
+    win.webContents.openDevTools();
+  } else {
+    // Vite -> dist, CRA -> zmień na '../build/index.html'
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
